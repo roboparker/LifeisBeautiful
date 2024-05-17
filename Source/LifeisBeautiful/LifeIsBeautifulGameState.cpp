fb397958c8785b1b00c435857dffc990c1772498
Robert Parker <robo.parker@proton.me>
2024-05-17 16:28:07 -0500

    Add time and bindable events to the game state

---
+++ Source/LifeisBeautiful/LifeIsBeautifulGameState.cpp
@@ -0,0 +1,25 @@
+﻿#include "LifeIsBeautifulGameState.h"
+
+void ALifeIsBeautifulGameState::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	FGameTimeStruct OldTime = CurrentTime;
+
+	CurrentTime.AddSeconds(DeltaTime * TimeDilation);
+
+	if (CurrentTime.Minute != OldTime.Minute)
+	{
+		OnMinuteChanged.Broadcast(CurrentTime);
+	}
+
+	if (CurrentTime.Hour != OldTime.Hour)
+	{
+		OnHourChanged.Broadcast(CurrentTime);
+	}
+
+	if (CurrentTime.Day != OldTime.Day)
+	{
+		OnDayChanged.Broadcast(CurrentTime);
+	}
+}
---
+++ Source/LifeisBeautiful/LifeIsBeautifulGameState.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameState.h"
+#include "Time/FGameTimeStruct.h"
+#include "LifeIsBeautifulGameState.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnMinuteChanged, FGameTimeStruct, CurrentTime);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnHourChanged, FGameTimeStruct, CurrentTime);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnDayChanged, FGameTimeStruct, CurrentTime);
+
+UCLASS()
+class LIFEISBEAUTIFUL_API ALifeIsBeautifulGameState : public AGameState
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Game Time", meta = (AllowPrivateAccess = "true"))
+	FGameTimeStruct CurrentTime;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Game Time", meta = (AllowPrivateAccess = "true"))
+	float TimeDilation = 1.0f;
+
+	virtual void Tick(float DeltaTime) override;
+
+public:
+	UPROPERTY(BlueprintAssignable, Category = "Game Time")
+	FOnMinuteChanged OnMinuteChanged;
+
+	UPROPERTY(BlueprintAssignable, Category = "Game Time")
+	FOnHourChanged OnHourChanged;
+
+	UPROPERTY(BlueprintAssignable, Category = "Game Time")
+	FOnDayChanged OnDayChanged;
+
+};
