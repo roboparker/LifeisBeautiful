4eb51a8dc945a660ba6cb83768855e3c5db0fbcc
Robert Parker <robo.parker@proton.me>
2024-05-17 15:51:00 -0500

    Create enums for days of week and seasons of year

---
+++ Source/LifeisBeautiful/Days.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/UserDefinedEnum.h"
+#include "Days.generated.h"
+
+UENUM(BlueprintType)
+enum class EDays : uint8
+{
+	LUMIN UMETA(DisplayName = "Lumin"),
+	TERRAN UMETA(DisplayName = "Terran"),
+	AQUAEN UMETA(DisplayName = "Aquaen"),
+	SYLVAN UMETA(DisplayName = "Sylvan"),
+	PYRON UMETA(DisplayName = "Pyron"),
+	UMBRA UMETA(DisplayName = "Umbra"),
+};
---
+++ Source/LifeisBeautiful/Seasons.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/UserDefinedEnum.h"
+#include "Seasons.generated.h"
+
+UENUM(BlueprintType)
+enum class ESeasons : uint8
+{
+	DAWNLIGHT UMETA(DisplayName = "Dawnlight"),
+	SUNCREST UMETA(DisplayName = "Suncrest"),
+	ELDERBLOOM UMETA(DisplayName = "Elderbloom"),
+	DUSKTIDE UMETA(DisplayName = "Dusktide")
+};
