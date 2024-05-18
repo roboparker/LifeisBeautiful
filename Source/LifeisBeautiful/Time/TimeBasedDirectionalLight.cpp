01466180dcf993c165f1468a85b6921352b8a0b6
Robert Parker <robo.parker@proton.me>
2024-05-18 17:14:43 -0500

    directional light based on game time

---
+++ Source/LifeisBeautiful/Time/TimeBasedDirectionalLight.cpp
@@ -0,0 +1,28 @@
+﻿#include "TimeBasedDirectionalLight.h"
+
+#include "Components/DirectionalLightComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "../LifeisBeautifulGameState.h"
+
+ATimeBasedDirectionalLight::ATimeBasedDirectionalLight()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	DirectionalLight = CreateDefaultSubobject<UDirectionalLightComponent>(TEXT("DirectionalLight"));
+	RootComponent = DirectionalLight;
+}
+
+void ATimeBasedDirectionalLight::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (ALifeIsBeautifulGameState* GameState = Cast<ALifeIsBeautifulGameState>(UGameplayStatics::GetGameState(GetWorld())))
+	{
+		GameState->OnMinuteChanged.AddDynamic(this, &ATimeBasedDirectionalLight::OnMinuteChanged);
+	}
+	
+}
+
+void ATimeBasedDirectionalLight::OnMinuteChanged(FGameTimeStruct CurrentTime)
+{
+	DirectionalLight->AddLocalRotation(FRotator(360.0f / 1440.0f, 0, 0));
+}
---
+++ Source/LifeisBeautiful/Time/TimeBasedDirectionalLight.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "FGameTimeStruct.h"
+#include "GameFramework/Actor.h"
+#include "TimeBasedDirectionalLight.generated.h"
+
+UCLASS()
+class LIFEISBEAUTIFUL_API ATimeBasedDirectionalLight : public AActor
+{
+	GENERATED_BODY()
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Light", meta = (AllowPrivateAccess = "true"))
+	UDirectionalLightComponent* DirectionalLight;
+
+	UFUNCTION()
+	void OnMinuteChanged(FGameTimeStruct CurrentTime);
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	ATimeBasedDirectionalLight();
+};
