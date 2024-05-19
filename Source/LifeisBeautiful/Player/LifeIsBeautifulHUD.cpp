730c18654363a8ca508d5dcd579635694d850aab
Robert Parker <robo.parker@proton.me>
2024-05-19 18:25:04 -0500

    create hud class

---
+++ Source/LifeisBeautiful/Player/LifeIsBeautifulHUD.cpp
@@ -0,0 +1,1 @@
+﻿#include "LifeIsBeautifulHUD.h"
---
+++ Source/LifeisBeautiful/Player/LifeIsBeautifulHUD.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "LifeIsBeautifulHUD.generated.h"
+
+UCLASS()
+class LIFEISBEAUTIFUL_API ALifeIsBeautifulHUD : public AHUD
+{
+	GENERATED_BODY()
+};
