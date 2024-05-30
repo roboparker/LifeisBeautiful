93f9f16b9f1167cb7eb18cc5c8ea39e2b4fca5da
Robert Parker <robo.parker@proton.me>
2024-05-30 14:15:51 -0500

    create base player controller class

---
+++ Source/LifeisBeautiful/Player/LifeIsBeautifulPlayerController.cpp
@@ -0,0 +1,11 @@
+﻿#include "LifeIsBeautifulPlayerController.h"
+#include "LifeIsBeautifulCharacter.h"
+
+void ALifeIsBeautifulPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	PlayerCharacter = Cast<ALifeIsBeautifulCharacter>(GetPawn());
+	if(!PlayerCharacter)
+		UE_LOG(LogTemp, Error, TEXT("PlayerCharacter is not of type ALifeIsBeautifulCharacter"));
+}
---
+++ Source/LifeisBeautiful/Player/LifeIsBeautifulPlayerController.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "LifeIsBeautifulPlayerController.generated.h"
+
+class ALifeIsBeautifulCharacter;
+
+UCLASS()
+class LIFEISBEAUTIFUL_API ALifeIsBeautifulPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+	UPROPERTY(BlueprintReadOnly, Category = "Controller", meta = (AllowPrivateAccess = "true"))
+	ALifeIsBeautifulCharacter* PlayerCharacter;
+
+protected:
+	virtual void BeginPlay();
+
+public:
+	ALifeIsBeautifulCharacter* GetPlayerCharacter() const { return PlayerCharacter; }
+};
