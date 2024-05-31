eb543f0adc84b9463ca0b896423c131d8fae1df3
Robert Parker <robo.parker@proton.me>
2024-05-31 15:19:02 -0500

    Create game instance

---
+++ Source/LifeisBeautiful/Game/LifeIsBeautifulGameInstance.cpp
@@ -0,0 +1,1 @@
+﻿#include "LifeIsBeautifulGameInstance.h"
---
+++ Source/LifeisBeautiful/Game/LifeIsBeautifulGameInstance.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "LifeIsBeautifulGameInstance.generated.h"
+
+UCLASS()
+class LIFEISBEAUTIFUL_API ULifeIsBeautifulGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+};
