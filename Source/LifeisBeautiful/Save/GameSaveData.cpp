e714471b74fc430b9432dc39e2b63a298603b7d4
Robert Parker <robo.parker@proton.me>
2024-06-01 06:19:57 -0500

    use guard clauses with logs

--- Source/LifeisBeautiful/Save/GameSaveData.cpp
+++ Source/LifeisBeautiful/Save/GameSaveData.cpp
@@ -17,15 +17,16 @@ void UGameSaveData::SaveGame() const
 	FAsyncSaveGameToSlotDelegate SavedDelegate;
 	SavedDelegate.BindLambda([this](const FString& SlotName, const int32 UserIndex, bool bSuccess)
 	{
-		if (bSuccess)
+		if (!bSuccess)
 		{
-			UE_LOG(LogTemp, Log, TEXT("Game saved successfully"));
-			OnSaveGameComplete.Broadcast();
-		}
-		else
-		{
+			FFrame::KismetExecutionMessage(TEXT("Failed to save game"), ELogVerbosity::Error);
 			UE_LOG(LogTemp, Error, TEXT("Failed to save game"));
+
+			return;
 		}
+
+		UE_LOG(LogTemp, Log, TEXT("Game saved successfully"));
+		OnSaveGameComplete.Broadcast();
 	});
 
 	UGameplayStatics::AsyncSaveGameToSlot (SaveGameInstance, SaveGameInstance->SlotName, SaveGameInstance->UserIndex, SavedDelegate);
@@ -33,18 +34,27 @@ void UGameSaveData::SaveGame() const
 
 void UGameSaveData::LoadGame() const
 {
+	if(!UGameplayStatics::DoesSaveGameExist(SlotName, UserIndex))
+	{
+		FFrame::KismetExecutionMessage(TEXT("No save data found to load"), ELogVerbosity::Warning);
+		UE_LOG(LogTemp, Warning, TEXT("No save data found to load"));
+
+		return;
+	}
+
 	FAsyncLoadGameFromSlotDelegate LoadedDelegate;
 	LoadedDelegate.BindLambda([this](const FString&, const int32, USaveGame* LoadedGameData)
 	{
-		if (LoadedGameData)
+		if (!LoadedGameData)
 		{
-			UE_LOG(LogTemp, Log, TEXT("Game loaded successfully"));
-			OnLoadGameComplete.Broadcast(LoadedGameData);
-		}
-		else
-		{
+			FFrame::KismetExecutionMessage(TEXT("Failed to load game"), ELogVerbosity::Error);
 			UE_LOG(LogTemp, Error, TEXT("Failed to load game"));
+
+			return;
 		}
+
+		UE_LOG(LogTemp, Log, TEXT("Game loaded successfully"));
+		OnLoadGameComplete.Broadcast(LoadedGameData);
 	});
 
 	UGameplayStatics::AsyncLoadGameFromSlot(SlotName, UserIndex, LoadedDelegate);
@@ -52,14 +62,15 @@ void UGameSaveData::LoadGame() const
 
 void UGameSaveData::DeleteGame() const
 {
-	if (UGameplayStatics::DoesSaveGameExist(SlotName, UserIndex))
+	if (!UGameplayStatics::DoesSaveGameExist(SlotName, UserIndex))
 	{
-		UGameplayStatics::DeleteGameInSlot(SlotName, UserIndex);
-		UE_LOG(LogTemp, Log, TEXT("Game save data deleted successfully"));
-		OnSaveGameDeleted.Broadcast();
+		FFrame::KismetExecutionMessage(TEXT("No save data found to delete"), ELogVerbosity::Error);
+		UE_LOG(LogTemp, Error, TEXT("No save data found to delete"));
+
+		return;
 	}
-	else
-	{
-		UE_LOG(LogTemp, Warning, TEXT("No save data found to delete"));
-	}
+
+	UGameplayStatics::DeleteGameInSlot(SlotName, UserIndex);
+	UE_LOG(LogTemp, Log, TEXT("Game save data deleted successfully"));
+	OnSaveGameDeleted.Broadcast();
 }
